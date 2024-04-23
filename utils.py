5fba08601eb8d78c8618410d4c4cac5d42bcad67
clovisdyarx <clovis.lechien@epita.fr>
2024-04-23 12:47:17 +0000

    fix v2

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 import s3fs
 import zipfile
-import glob
 
 
 def get_dataframe():
@@ -13,10 +12,10 @@ def get_dataframe():
     # Télécharger les données dans le service
     PATH_IN = 'gvimont/diffusion/hackathon-minarm-2024/AIVSAI/HC3.zip'
     fs.download(PATH_IN, 'data/HC3.zip')
-    with zipfile.ZipFile("data/HC3.zip","r") as zip_file:
+    with zipfile.ZipFile("data/HC3.zip", "r") as zip_file:
         zip_file.extractall("data/")
 
-    #Concatenate subsets
+    # Concatenate subsets
     df = pd.DataFrame()
     for file in ['finance', 'open_qa', 'medicine', 'wiki_csai']:
         df = pd.concat([df, pd.read_json("data/HC3/" + file + ".jsonl", lines=True)])
